305e53563a9b202ca5d8285b04880877facd9648
Dan Goldsmith <dan@d2g.org.uk>
2015-04-15 14:47:04 +0100

    Updated iota.

--- capture.go
+++ capture.go
@@ -341,21 +341,18 @@ func (l *Logger) Write(p []byte) (int, error) {
 }
 
 // Cheap integer to fixed-width decimal ASCII.  Give a negative width to avoid zero-padding.
-// Knows the buffer has capacity.
 func itoa(buf *[]byte, i int, wid int) {
-	var u uint = uint(i)
-	if u == 0 && wid <= 1 {
-		*buf = append(*buf, '0')
-		return
+	// Assemble decimal in reverse order.
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || wid > 1 {
+		wid--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
 	}
 
-	// Assemble decimal in reverse order.
-	var b [32]byte
-	bp := len(b)
-	for ; u > 0 || wid > 0; u /= 10 {
-		bp--
-		wid--
-		b[bp] = byte(u%10) + '0'
-	}
+	b[bp] = byte('0' + i)
 	*buf = append(*buf, b[bp:]...)
 }
